9150b31b7de7a8c119168fab112104128353bf64
Abhishek Patwardhan <abhikota1@gmail.com>
2022-02-03 00:53:39 +0530

    Created Notes,User models and notes,auth apis

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+//routes
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
---
+++ backend/routes/notes.js
@@ -0,0 +1,11 @@
+// /api/notes is handled here 
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    const obj={
+    }
+    res.json(obj);
+})
+
+module.exports = router;
