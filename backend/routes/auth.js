5435bc6275602c4e0ece552cde4d6ed168f02e85
Abhishek Patwardhan <abhikota1@gmail.com>
2022-02-05 00:08:21 +0530

    auth api validation before storing to db

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,13 +2,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../database/models/User');
+const { body, validationResult } = require('express-validator');
 
 // Create User : POST '/api/auth/'
-router.post('/',(req,res)=>{
-    const user = User(req.body)
-    user.save();
-    console.log(req.body);
-    res.send(req.body);
-})
+router.post('/',
+    body('email','Enter a valid Email').isEmail(),
+    body('password','Password must be atleast 5 characters').isLength({min:5}),
+    (req,res)=>{
+        const errors=validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        User.create({
+            name: req.body.name,
+            email:req.body.email,
+            password: req.body.password,
+        }).then(user => res.json(user)).catch(err=>{
+            console.log(err)
+            res.json({error:'this email already exists',message:err.message})
+        });
+    })
 
 module.exports = router;
