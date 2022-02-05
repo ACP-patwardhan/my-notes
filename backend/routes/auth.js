e7b2eb3265c02cd190dc0da7cda08bcc5fceecae
Abhishek Patwardhan <abhikota1@gmail.com>
2022-02-05 22:52:42 +0530

    created login api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,8 +5,9 @@ const User = require('../database/models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
 // Create User : POST '/api/auth/createUser'
-router.post('/createUser/',
+router.post('/createUser',
     body('email', 'Enter a valid Email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
     async (req, res) => {
@@ -16,15 +17,14 @@ router.post('/createUser/',
             return res.status(400).json({ errors: errors.array() });
         }
 
-        //check unique email.
         try {
+            //check unique email.
             const emailExists = await User.findOne({ email: req.body.email });
             if (emailExists) {
-                res.status(400).json({ error: 'User with this email already exists' });
-                return;
+                return res.status(400).json({ error: 'User with this email already exists' });
             }
             //encrypt password using brcrypt hash
-            const hash_password = await bcrypt.hash(req.body.password,10);
+            const hash_password = await bcrypt.hash(req.body.password, 10);
             let user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
@@ -32,18 +32,56 @@ router.post('/createUser/',
             })
             //generate auth token.
             const tokenPayload = {
-                user:{
+                user: {
                     id: user.id
                 }
             }
-            const authToken = jwt.sign(tokenPayload,'secreteStringisGod');
-            res.json({authToken});
+            const authToken = jwt.sign(tokenPayload, 'secreteStringisGod');
+            res.json({ authToken });
 
         } catch (error) {
             console.error(error);
-            res.status(500).json({error:'Internal server error'})
+            res.status(500).json({ error: 'Internal server error' })
         }
 
     })
 
+// login : POST '/api/auth/login'
+router.post('/login',
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password required').exists(),
+    async (req, res) => {
+        //catch validation errors if any
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        
+        try {
+            //check unique email.
+            const user = await User.findOne({ email: req.body.email });
+            if (!user) {
+                return res.status(400).json({ error: 'Invalid credentials' });
+            }
+            //encrypt password using brcrypt hash
+            const checkPassword = await bcrypt.compare(req.body.password,user.password)
+            if(!checkPassword){
+                return res.status(400).json({ error: 'Invalid credentials' });
+            }
+            //generate auth token.
+            const tokenPayload = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(tokenPayload, 'secreteStringisGod');
+            res.json({ authToken });
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal server error' })
+        }
+
+    })
 module.exports = router;
